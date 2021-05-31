3211eb7b32acc07747c4d8d1a61bda5b14a9aa8e
syedumerali <umer.ali@salsoft.co>
2021-05-31 18:14:05 +0500

    unused styles are removed.

--- src/Components/Quantity/styles.js
+++ src/Components/Quantity/styles.js
@@ -1,38 +1,24 @@
-import {StyleSheet} from 'react-native';
-import vw from '../../Units/vw';
-import vh from '../../Units/vh';
-import {Fonts} from '../../assets/fonts';
+import { StyleSheet } from "react-native";
+import vw from "../../Units/vw";
+import vh from "../../Units/vh";
+import { Fonts } from "../../assets/fonts";
 
-import { store } from '../../WooCommerceWrapper/store';
-const state = store.getState()
-const config = state.ConfigReducer
-const {
-  primary_heading_color,
-  primary_section_color,
-  primary_font_color,
-  secondary_font_color,
-  primaryColor,
-  secondaryColor,
-  primary_placeholder_Color,
-  primary_border_color,
-  primary_background_color,
-  secondary_background_color,
-  primary_message_color,
-  drawer_Active_Color,
-  drawer_inActive_Color,
-  default_color,
-} = config
+import { store } from "../../WooCommerceWrapper/store";
+const state = store.getState();
+const config = state.ConfigReducer;
+const { primary_font_color, primary_border_color } = config;
+
 export default StyleSheet.create({
-  container: {flexDirection: 'row', alignItems: 'center'},
+  container: { flexDirection: "row", alignItems: "center" },
   btn: {
     backgroundColor: primary_border_color,
     width: vw * 6.5,
     height: vw * 6.5,
     borderRadius: vw * 3.75,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
-  icon: {height: vw * 3.5, width: vw * 3.5},
+  icon: { height: vw * 3.5, width: vw * 3.5 },
   text: {
     color: primary_font_color,
     fontSize: vh * 3,
